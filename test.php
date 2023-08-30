475447bfa25f0ff477e749ab5ad74e2de680658d
Ondrej Mirtes <ondrej@mirtes.cz>
2023-08-30 11:51:22 +0200

    More tests

--- test.php
+++ test.php
@@ -2,6 +2,15 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+class TestService
+{
+
+	public function __construct(public string $test)
+	{
+	}
+
+}
+
 $configurator = new Nette\Bootstrap\Configurator;
 $configurator->setTempDirectory(__DIR__ . '/temp');
 $configurator->addConfig(__DIR__ . '/test.neon');
@@ -11,3 +20,9 @@ $configurator->addDynamicParameters([
 $container = $configurator->createContainer();
 
 var_dump($container->parameters['test']);
+
+$one = $container->getService('one');
+var_dump($one->test);
+
+$two = $container->getService('two');
+var_dump($two->test);
